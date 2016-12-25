72eb08c384fa3ed12d77965ebb519435d05bb5b1
tarekgabarin <tarekgabarin1992@gmail.com>
2016-12-25 17:17:50 -0500

    poop

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,10 +6,9 @@ var Main = React.createClass({
   render: function () {
     return (
       <div>
-      <div>
-        <div>
-          <Navigation/>
-          <p>Main rendered</p>
+        <Navigation/>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
           {this.props.children}
         </div>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ resolve: {
     Navigation: "app/components/Navigation.jsx",
     Timer: "app/components/Timer.jsx",
     Countdown: "app/components/Countdown.jsx",
-    Clock: "app/components/Clock.jsx"
+    Clock: "app/components/Clock.jsx",
+    CountdownForm: "app/components/CountdownForm.jsx"
   },
   extensions: ['', '.js', 'jsx']
 },
