470fac4d7f5e8bd5d798b9b60eedcc0ff414794f
tarekgabarin <tarekgabarin1992@gmail.com>
2016-12-25 16:11:52 -0500

    create clock component

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ resolve: {
     applicationStyles: 'app/styles/app.scss',
     Navigation: "app/components/Navigation.jsx",
     Timer: "app/components/Timer.jsx",
-    Countdown: "app/components/Countdown.jsx"
+    Countdown: "app/components/Countdown.jsx",
+    Clock: "app/components/Clock.jsx"
   },
   extensions: ['', '.js', 'jsx']
 },
