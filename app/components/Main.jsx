2f89bfb01e1f83262692325ad310b6e4b2becfce
tarekgabarin <tarekgabarin1992@gmail.com>
2016-12-25 13:00:44 -0500

    messagedfsefsdfsd

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var Navigation =require('Navigation');
+
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div>
+      <div>
+        <div>
+          <Navigation/>
+          <p>Main rendered</p>
+          {this.props.children}
+        </div>
+      </div>
+      </div>
+    );
+  }
+})
+
+module.exports = Main;
